de140e20ae4451c76b026f1c09a4d8bed0f3c12d
jmarinfi <jmarinfi@uoc.edu>
2023-04-09 15:08:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CHAPTER 8
+
+Implementación de los ejemplos del capítulo 8 del libro **Building Data Science Applications with FastAPI**.
---
+++ echo/app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from starlette.websockets import WebSocket, WebSocketDisconnect
+
+app = FastAPI()
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message text was: {data}")
+    except WebSocketDisconnect:
+        await websocket.close()
